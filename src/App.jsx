4d3df9a29b1dbf45baaedb071f9149375ebf5553
yosuva4 <yosuvaitech@gmail.com>
2024-03-19 16:19:40 +0530

    yosuva first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Footer from "./mainlayout/Footer";
+import Header from "./mainlayout/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mainlayout/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <h1 className="font-roboto bg-primary text-secondary">Footer</h1>
+    </div>
+  );
+}
