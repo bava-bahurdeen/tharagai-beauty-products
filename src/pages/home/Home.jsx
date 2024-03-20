da36a5a0d2042a124fbfb7775c4a38a411e457e2
yosuva4 <yosuvaitech@gmail.com>
2024-03-20 11:13:56 +0530

    yosuva commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import banner1 from "../../assets/images/hero/b1.jpg";
+import banner2 from "../../assets/images/hero/b2.jpg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="w-full h-full"
+      >
+        <SwiperSlide>
+          <img src={banner1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
