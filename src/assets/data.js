49a614fa46bf0778f0e60d274b7069d7e0ad56de
abdrhmnjgrdr <abdurrahamanjagirdhar2412@gmail.com>
2024-04-04 19:32:50 +0530

    Update

---
+++ src/assets/data.js
@@ -0,0 +1,103 @@
+import P1 from "../assets/images/products/P1.png"
+import P2 from "../assets/images/products/P2.png"
+import P3 from "../assets/images/products/P3.png"
+import P4 from "../assets/images/products/P4.png"
+import P5 from "../assets/images/products/P5.png"
+import P6 from "../assets/images/products/P6.png"
+import P7 from "../assets/images/products/P7.png"
+import P8 from "../assets/images/products/P8.png"
+
+export const product = [
+    {
+      id: 1,
+      image: P1,
+      category: "Bath & Body",
+      title : "Avocado extract shower gel",
+      price:[
+        {
+          oldprice: 16.25,
+          newPrice: 12.90
+        }
+      ]
+    },
+    {
+      id: 2,
+      image: P2,
+      category: "Bath & Body",
+      title : "Body oil anti-cellulite",
+      price:[
+        {
+          newPrice: 13.25
+        }
+      ]
+    },
+    {
+      id: 3,
+      image: P3,
+      category: "Bath & Body",
+      title : "Hair repair shampoo",
+      price:[
+        {
+          oldprice: 16.25,
+          newPrice: 13.90
+        }
+      ]
+    },
+    {
+      id: 4,
+      image: P4,
+      category: "Bath & Body",
+      title : "Moisturizing conditioner",
+      price:[
+        {
+          newPrice: 15.50
+        }
+      ]
+    },
+    {
+      id: 5,
+      image: P5,
+      category: "Bath & Body",
+      title : "Phytoactive hydra-firm mask",
+      price:[
+        {
+          newPrice: 19.50
+        }
+      ]
+    },
+    {
+      id: 6,
+      image: P6,
+      category: "Bath & Body",
+      title : "Rapid response detox mask",
+      price:[
+        {
+          oldprice: 16.25,
+          newPrice: 10.50
+        }
+      ]
+    },
+    {
+      id: 7,
+      image: P7,
+      category: "Bath & Body",
+      title : "Replumping hair conditioner",
+      price:[
+        {
+          oldprice: 16.25,
+          newPrice: 13.90
+        }
+      ]
+    },
+    {
+      id: 8,
+      image: P8,
+      category: "Bath & Body",
+      title : "Tropical rehab shampoo",
+      price:[
+        {
+          newPrice: 14.90
+        }
+      ]
+    },
+  ]
---
+++ src/pages/categories/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card({i}) {
+   
+  return (
+    <div  className=''>
+        <h1>{i.title}</h1>
+    </div>
+  )
+}
+
+export default Card
