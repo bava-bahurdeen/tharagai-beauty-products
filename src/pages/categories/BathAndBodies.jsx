45e7394983cebd986096f59f20ce0068bfedf6df
abdrhmnjgrdr <abdurrahamanjagirdhar2412@gmail.com>
2024-04-04 21:40:28 +0530

    updated bath and body page by abdul

--- src/pages/categories/BathAndBodies.jsx
+++ src/pages/categories/BathAndBodies.jsx
@@ -2,40 +2,39 @@ import React from "react";
 import { product } from "../../assets/data";
 import Card from "./Card";
 
-
 function BathAndBodies() {
-  
   return (
     <>
-      <section className="bg-success/25 container mx-auto pt-10 pb-10">
-        <div className="">
-          <h1 className="text-5xl">Bath & Body </h1>
-          <p className="mt-14 text-[15px] font-light">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Dolore eu fugiat nulla pariatur
-            sint occaecat cupidatat non proident sunt in culpa qui officia
-            deserunt.
-          </p>
-        </div>
-        <div className="mt-14 text-[15px] font-light flex justify-between">
-          <h6 className="font-light">Showing all results</h6>
-          <select name="Sorting" id="" className="bg-transparent ">
-            <option value="">Default sorting</option>
-            <option value="">Sort by Popularity</option>
-            <option value="">Sort by Average Rating</option>
-            <option value="">Sort by Latest</option>
-            <option value="">Sort by Price: Low to High</option>
-            <option value="">Sort by Price: High to low</option>
-          </select>
-        </div>
-        <div className="mt-14">
-          {
-            product.map((i)=>{
-              return(<Card i={i} key={i.id}/>)
-            })
-          }
+      <section className="bg-primary">
+        <div className="container mx-auto pt-10 pb-10">
+        <p className="text-black/40 text-xs">Home / Bath & Body</p>
+          <div className="mt-8">
+            <h1 className="text-5xl">Bath & Body </h1>
+            <p className="mt-14 text-sm font-light">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Dolore eu fugiat nulla
+              pariatur sint occaecat cupidatat non proident sunt in culpa qui
+              officia deserunt.
+            </p>
+          </div>
+          <div className="mt-14 text-sm font-light md:flex  justify-between">
+            <h6 className="font-light">Showing all results</h6>
+            <select name="Sorting" className="bg-transparent mt-3 outline-none ">
+              <option value="">Default sorting</option>
+              <option value="">Sort by Popularity</option>
+              <option value="">Sort by Average Rating</option>
+              <option value="">Sort by Latest</option>
+              <option value="">Sort by Price: Low to High</option>
+              <option value="">Sort by Price: High to low</option>
+            </select>
+          </div>
+          <div className="mt-14 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+            {product.map((i) => {
+              return <Card i={i} key={i.id} />;
+            })}
+          </div>
         </div>
       </section>
     </>
--- src/pages/categories/Card.jsx
+++ src/pages/categories/Card.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
-function Card({i}) {
-   
+function Card({ i }) {
+  const { id, image, title, category, price } = i;
+
   return (
-    <div  className=''>
-        <h1>{i.title}</h1>
+    <div className="relative group">
+      <img src={image} alt={title} className="w-[290px] rounded-sm" />
+      <div className="mt-5 ">
+        <p className="text-xs text-black/40">{category}</p>
+        <h6 className="text-sm">{title}</h6>
+        <div className="flex gap-1 text-xs ">
+          {price.oldprice && (
+            <del className="text-black/40">${price.oldprice}</del>
+          )}
+          <p className="font-semibold">${price.newPrice}</p>
+        </div>
+        <div className="absolute top-5 right-5 bg-white rounded-full shadow-lg p-1 opacity-0 group-hover:opacity-100 transition-all duration-400">
+          <FaShoppingCart />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
