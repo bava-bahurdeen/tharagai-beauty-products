b86573d5dd3804c25245dcac471760b4baba2ac8
yosuva4 <yosuvaitech@gmail.com>
2024-04-12 01:20:58 +0530

    Api get

--- src/pages/categories/ProductDetails.jsx
+++ src/pages/categories/ProductDetails.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import p1 from "../../assets/images/products/P1.png";
 import { product } from "../../assets/data";
 export default function Productpage() {
-    const [toggle,Settoggle]=useState(1)
-  
+  const [toggle, Settoggle] = useState(1);
+
   return (
     <>
       <div className="bg-primary p-0 md:p-12 overflow-hidden">
@@ -44,7 +44,6 @@ export default function Productpage() {
                 <input
                   type="number"
                   className="max-w-full border-2 border-b p-3 text-dark_gray  w-[54px] h-[35px]"
-                  
                 />
                 <button className="bg-secondary w-[230px] h-[34px] ">
                   Add To Cart
@@ -62,45 +61,90 @@ export default function Productpage() {
           </div>
           {/* tab section */}
           <div className="flex gap-2 sm:gap-5">
-            <button onClick={()=>{Settoggle(1)}} className={`${toggle==1?"border-b-2 border-black text-[13px] sm:text-[20px] ":"border-none"}`}>Description</button>
-            <button onClick={()=>{Settoggle(2)}} className={`${toggle==2?"  border-b-2 border-black text-[13px] sm:text-[20px] ":"border-none hidden sm:block"}`}>Addition Information</button>
-            <button onClick={()=>{Settoggle(3)}} className={`${toggle==3?"border-b-2 border-black text-[13px] sm:text-[20px] ":"border-none"}`}>Reviews</button>
+            <button
+              onClick={() => {
+                Settoggle(1);
+              }}
+              className={`${
+                toggle == 1
+                  ? "border-b-2 border-black text-[13px] sm:text-[20px] "
+                  : "border-none"
+              }`}
+            >
+              Description
+            </button>
+            <button
+              onClick={() => {
+                Settoggle(2);
+              }}
+              className={`${
+                toggle == 2
+                  ? "  border-b-2 border-black text-[13px] sm:text-[20px] "
+                  : "border-none hidden sm:block"
+              }`}
+            >
+              Addition Information
+            </button>
+            <button
+              onClick={() => {
+                Settoggle(3);
+              }}
+              className={`${
+                toggle == 3
+                  ? "border-b-2 border-black text-[13px] sm:text-[20px] "
+                  : "border-none"
+              }`}
+            >
+              Reviews
+            </button>
           </div>
           <div>
-            <p className={toggle===1?"block font-[15px] mt-8 text-sm ":"hidden"} >
+            <p
+              className={
+                toggle === 1 ? "block font-[15px] mt-8 text-sm " : "hidden"
+              }
+            >
               Description Lorem ipsum dolor sit amet consectetur adipisicing
               elit. Eaque minima, rem suscipit aperiam veritatis distinctio
               debitis nobis in dolorum provident odit porro perferendis esse
               nisi culpa ipsam atque totam consequuntur!
             </p>
-            <p className={toggle===2?"block font-[15px] mt-8 text-sm ":"hidden"}>
-              Addition Information Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Eaque minima, rem suscipit aperiam veritatis distinctio
-              debitis nobis in dolorum provident odit porro perferendis esse
-              nisi culpa ipsam atque totam consequuntur!
+            <p
+              className={
+                toggle === 2 ? "block font-[15px] mt-8 text-sm " : "hidden"
+              }
+            >
+              Addition Information Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Eaque minima, rem suscipit aperiam veritatis
+              distinctio debitis nobis in dolorum provident odit porro
+              perferendis esse nisi culpa ipsam atque totam consequuntur!
             </p>
-            <p className={toggle===3?"block font-[15px] mt-8 text-sm":"hidden"}>
-              Reviews Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Eaque minima, rem suscipit aperiam veritatis distinctio
-              debitis nobis in dolorum provident odit porro perferendis esse
-              nisi culpa ipsam atque totam consequuntur!
+            <p
+              className={
+                toggle === 3 ? "block font-[15px] mt-8 text-sm" : "hidden"
+              }
+            >
+              Reviews Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Eaque minima, rem suscipit aperiam veritatis distinctio debitis
+              nobis in dolorum provident odit porro perferendis esse nisi culpa
+              ipsam atque totam consequuntur!
             </p>
           </div>
           <div>
-         
-<h1 className="font-lexend  mt-8  sm:mt-6 sm:text-[40px] text-[20px]  font-medium">Related products</h1>
-       {product.map((items)=>{
-        <div key={items.id} className="flex">
-            <img src={items.image} alt="product" />
-            <h1>{items.category}</h1>
-            <h1>{items.title}</h1>
-           <div className="flex">
-            <p>{items.price[0]}</p>
-            <p>{items.price[1]}</p>
-           </div>
-                   </div>
-       })}
-
+            <h1 className="font-lexend  mt-8  sm:mt-6 sm:text-[40px] text-[20px]  font-medium">
+              Related products
+            </h1>
+            {product.map((items) => {
+              <div key={items.id} className="flex">
+                <img src={items.image} alt="product" />
+                <h1>{items.category}</h1>
+                <h1>{items.title}</h1>
+                <div className="flex">
+                  <p>{items.price[0]}</p>
+                  <p>{items.price[1]}</p>
+                </div>
+              </div>;
+            })}
           </div>
         </section>
       </div>
